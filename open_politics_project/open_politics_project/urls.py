c93df415d6978812f25fe1602ec44c0c67711167
JimVincentW <jimvwagner@googlemail.com>
2023-10-11 10:00:10 +0000

    models, api, urls

--- open_politics_project/open_politics_project/urls.py
+++ open_politics_project/open_politics_project/urls.py
@@ -21,9 +21,10 @@ from bt_thema import views, urls
 from news import views, urls
 
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('process/', views.process_view, name='process'),
+    #path('process/', views.process_view, name='process'),
     path('receive-url/', views.receive_url, name='receive_url'),
     path('receive-url/<int:id>/', views.receive_url, name='receive_url'),
     re_path(r'^\.well-known/acme-challenge/(?P<path>.+)$', serve, {
