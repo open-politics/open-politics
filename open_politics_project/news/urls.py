58e08571fe8cc7e6ad36cf6465d0b6d8b23448a2
Jim Wagner <jimvwagner@googlemail.com>
2023-10-12 12:14:47 +0200

    first ml/ nlp solution inbound

--- open_politics_project/news/urls.py
+++ open_politics_project/news/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('test-button', views.test_button, name="test_button"),
     path('api/fetch_articles/', views.fetch_articles_api, name='fetch_articles_api'),
     path('articles/', ArticleListView.as_view(), name='article_list'),
+    path('visualize/', views.visualize_entity_article_connections, name='visualize_entity_article_connections'),
+    
+
 ]
--- open_politics_project/news/views.py
+++ open_politics_project/news/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+import networkx as nx
 from django.http import JsonResponse
 from news.api_calls import call_with_search_parameters
+import matplotlib.pyplot as plt
+import io
 from news.models import NewsArticle, NewsSource
 from django.views.generic import ListView
+from django.http import FileResponse
+
+import matplotlib
+matplotlib.use('Agg')
+
 
 def fetch_articles_api(request):
     default_options = {
@@ -35,5 +43,30 @@ def news_home(request):
     return render(request, "news/news_home.html")
 
 
+def visualize_entity_article_connections(article):
+    # Sample entities (In a real use-case, you would dynamically extract entities from your articles or use some external service)
+    entities = [
+        {'word': 'Israel', 'score': 0.85},
+        {'word': 'Gaza', 'score': 0.61}
+    ]
+
+    # Fetch articles from the database
+    articles_data = NewsArticle.objects.all()[:10]  # Fetching first 10 articles, you can adjust this as per your requirements
+    articles = [article.title for article in articles_data]
+
+    G = nx.Graph()
+
+    # (rest of the function as before to generate the visualization...)
+
+    # Save figure to a BytesIO object
+    buf = io.BytesIO()
+    plt.savefig(buf, format="PNG")
+    buf.seek(0)
+
+    return FileResponse(buf, as_attachment=True, filename='network_graph.png')
 
 
+
+def get_image(request):
+    image_url = visualize_entity_article_connections()
+    return render(request, 'image_template.html', {'image_url': image_url})
