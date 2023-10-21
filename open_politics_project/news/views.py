12f27dd61d738fb71869aeadc3e3104b407b8d78
Jim Wagner <jimvwagner@googlemail.com>
2023-10-21 03:25:24 +0200

    added vision for website

--- open_politics_project/news/views.py
+++ open_politics_project/news/views.py
@@ -26,8 +26,12 @@ class ArticleListView(ListView):
     template_name = 'news/articles_list.html'
     context_object_name = 'articles'
 
+# ############################################################################################################
+### News 
 
-# News API
+## Helper functions
+
+# News from the last 24 hours
 def get_news_data(query, pageSize=40, sources_choice=None):
     sources_choice = 'all'
     if sources_choice == 'all' or sources_choice is None:
@@ -67,6 +71,7 @@ def get_news_data(query, pageSize=40, sources_choice=None):
     print(df)
     return df
 
+# Helper function to generate a list of Wikipedia summaries for a list of actors
 def generate_wikipedia_summaries(actors_string):
     # Split the actors_string by newline characters and strip spaces
     actors = [actor.strip() for actor in actors_string.split('\n') if actor.strip()]
@@ -82,7 +87,7 @@ def generate_wikipedia_summaries(actors_string):
 
     return actor_summaries
 
-# Wikipedia API
+# Wikipedia API Request
 def get_wikipedia_summary(actor_name):
     endpoint = "https://en.wikipedia.org/w/api.php"
     parameters = {
@@ -100,33 +105,7 @@ def get_wikipedia_summary(actor_name):
     for page_id, page_content in pages.items():
         return page_content.get("extract", "")
 
-# OpenAI API - News Summary
-def generate_synopsis(topic, df):
-    print("generate_synopsis")
-    articles = []
-    for index, row in df.iterrows():
-        articles.append(row['title'])
-    articles = ' '.join(articles)
-
-
-
-    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-    openai.api_key = OPENAI_API_KEY
-    if not OPENAI_API_KEY:
-        logger.error("OpenAI API Key not found!")
-    response = openai.ChatCompletion.create(
-        model="gpt-4",
-        messages = [{"role": "system", "content": 'You are a political news journalist tasked with Named Entity Recognition and Entity linking tasks. You return only an bullet point list in markdown'},
-                    {"role": "user", "content": "These following are the articles about" + topic + "from the last 24 hours. Please give a short summary about what is important right now to know about this matter. No more than 10 sentences and just the plain answer." },
-                    {"role": "assistant", "content" : "Ok, show me the articles please" },
-                    {"role": "user", "content": "Here are the articles:\n" + articles }],
-        stream=False,
-                                                                                                        )
-    result = response.choices[0]['message']['content']
-    print("synopsis generatd")
-    print(len(result))
-    return result
-
+# Helper function to parse the markdown list of actors
 def parse_actors(actors_string):
     # Split by newline
     lines = actors_string.strip().split("\n")
@@ -137,6 +116,41 @@ def parse_actors(actors_string):
     return parsed_actors
 
 
+
+# ############################################################################################################
+# Workin' LLMs
+
+
+def generate_synopsis(topic, df):
+    print("generate_synopsis")
+    articles = []
+    for index, row in df.iterrows():
+        articles.append(row['title'])
+    articles = ' '.join(articles)
+
+    print(articles)
+
+    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+    openai.api_key = OPENAI_API_KEY
+    if not OPENAI_API_KEY:
+        logger.error("OpenAI API Key not found!")
+    response = openai.ChatCompletion.create(
+        model="gpt-4",
+        messages = [{"role": "system", "content": 'You are an AI-driven news analyst programmed to extract and link named entities from textual data. You return only an bullet point list in markdown'},
+                    {"role": "user", "content": "Provide a concise summary of the primary events, key stakeholders, and strategic implications related to conflict/topic" + topic + "from the last 24 hours. Instructions: 1. Focus on high-level developments and strategic shifts. 2. Exclude individual anecdotes, isolated incidents, or stories unless they have broader strategic or symbolic significance. 3. Highlight any international involvements or reactions. 4. Prioritize information from reputable news sources and downplay tabloid or less verified news. 5. Offer a balanced perspective, taking into account various viewpoints. 6. No more than 10 sentences and just the plain answer." },
+                    {"role": "assistant", "content" : "Ok, show me the articles please" },
+                    {"role": "user", "content": "Here are the articles:\n" + articles }],
+        stream=False,
+                                                                                                        )
+    result = response.choices[0]['message']['content']
+    print("synopsis generatd")
+    print(len(result))
+    return result
+
+
+
+
+
 # OpenAI API - Political Actors
 def generate_actors(topic, df):
     print("extracting actors") 
@@ -145,12 +159,21 @@ def generate_actors(topic, df):
         articles.append(row['title'])
     articles = ' '.join(articles)
 
+    from transformers import pipeline
+
+    summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
+    
+    # articles = summarizer(articles, max_length=1024, min_length=30, do_sample=False)
+    # articles = articles[0]['summary_text']
+
+
+
 
     openai.api_key = os.getenv("OPENAI_API_KEY")
     response = openai.ChatCompletion.create(
         model="gpt-4",
-        messages = [{"role": "system", "content": 'You are a political news journalist tasked with Named Entity Recognition and Entity linking tasks. You return only a newline separated numbered list in markdown'},
-                    {"role": "user", "content": "These following are the articles about" + topic + "from the last 24 hours. Please write out political actors relevant to this matter. No more than 5 actors and just the plain answer." },
+        messages = [{"role": "system", "content": f'You are a political news journalist with expertise in Named Entity Recognition and Entity Linking. Your mission is to sift through news articles and identify the most pertinent political actors associated with a given topic, focusing on the context and relevance. Ensure that the list is selective, concise, and prioritizes the most significant entities. You return only an bullet point list in markdown'},
+                    {"role": "user", "content": "The following are articles about" + topic + "from the past 24 hours. Identify the top political actors from these articles, ensuring they hold substantial relevance to the topic. No more than 5 actors and just the plain answer." },
                     {"role": "assistant", "content" : "Ok, show me the articles please" },
                     {"role": "user", "content": "Here are the articles:\n" + articles }],
         stream=False,
@@ -161,7 +184,6 @@ def generate_actors(topic, df):
     print("actors generated")
     print(len(result))
     return result
-
 
 #Frontend rendering - News Summary
 def stream_synopsis(request):
@@ -203,27 +225,51 @@ def stream_actors(request):
     return render(request, 'news/actors.html', {'actors': actors_with_summaries})
 
 
-# # Frontend rendering - News Summary
+
+
+
+
+# ############################################################################################################
+# ############################################################################################################
+# ############################################################################################################
+# # Fake functions to save API costs while developing
+
+
+# #Frontend rendering - News Summary
 # def stream_synopsis(request):
 #     print("stream_synopsis")
 #     query = request.GET.get('query', None)  # Get the query from the GET parameters
 
-#     if not query:  # Check if the query is provided
-#         return render(request, 'news/synopsis.html', {})
+#     # if not query:  # Check if the query is provided
+#     #     return render(request, 'news/synopsis.html', {})
 
 #     # df = get_news_data(query)  # Fetch articles related to the query
 
 #     # synopsis = generate_synopsis(query, df)  # Generate the synopsis using OpenAI
 
 
-#     return render(request, 'news/synopsis.html', {'synopsis': 'synopsis \n'*20})
+#     return render(request, 'news/synopsis.html', {'synopsis': "While beeing in development, I can not account for the API costs. Thus this is a placeholder and is there to showcase the UI"})
 
 # # Frontend rendering - Political Actors
 # def stream_actors(request):
 #     print("streaming actors")
 #     query = request.GET.get('query', None)
 
-#     actors = ['Hamas', 'Benjamin Netanyahu', 'Joe Biden', 'Sen. Ted Cruz', 'Hezbollah'] 
-#     return render(request, 'news/actors.html', {'actors': actors})
+#     # df = get_news_data(query)
+#     # raw_actors_md = generate_actors(query, df)
+    
+#     # # Parsing the markdown list to get plain actor names
+#     # parsed_actors = parse_actors(raw_actors_md)
 
+#     # actors_with_summaries = []
 
+#     # # Fetch Wikipedia summary for each actor
+#     # for actor in parsed_actors:
+#     #     summary = get_wikipedia_summary(actor)
+#     #     actor_data = {
+#     #         'name': actor,
+#     #         'summary': summary
+#     #     }
+#     #     actors_with_summaries.append(actor_data)
+
+#     return render(request, 'news/actors.html', {'actors': [{"name": "Actor 1", "summary": "This is a summary of actor 1"}, {"name": "Actor 2", "summary": "This is a summary of actor 2"}]})
