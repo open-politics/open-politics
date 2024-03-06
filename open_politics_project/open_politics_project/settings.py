5357008ad736bbf543734197404bf296f9b12640
Jim Wagner <jimvwagner@googlemail.com>
2024-03-06 22:21:02 +0100

    frontend changes

--- open_politics_project/open_politics_project/settings.py
+++ open_politics_project/open_politics_project/settings.py
@@ -70,6 +70,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.db'  # or 'cache' for cache-backed sessions
