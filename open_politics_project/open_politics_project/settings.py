30828555b27b1baccd9aa05279c0e48a48326896
Jim Wagner <jimvwagner@googlemail.com>
2024-03-29 18:54:57 +0100

    changed django settings to not use environ

--- open_politics_project/open_politics_project/settings.py
+++ open_politics_project/open_politics_project/settings.py
@@ -11,11 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
 from pathlib import Path
-import environ
 
-# Read the .env file
-env = environ.Env()
-environ.Env.read_env()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
