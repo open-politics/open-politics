c64c48b65004b51ad9d508861492258b20b8030e
Jim Wagner <jimvwagner@googlemail.com>
2024-03-29 19:26:38 +0100

    backend & frontend updates

--- open_politics_project/open_politics_project/settings.py
+++ open_politics_project/open_politics_project/settings.py
@@ -10,8 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+import environ
 from pathlib import Path
 
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
