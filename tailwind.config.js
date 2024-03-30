113917d460af3d55f1633c17a05aba765421dde0
Jim Wagner <jimvwagner@googlemail.com>
2024-03-30 03:13:36 +0100

    major frontend pivot to tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './templates/**/*.html',
+    './open_politics_project/**/*.html',
+    // Add other directories where you have HTML files that use Tailwind CSS classes
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: 'class',
+}
