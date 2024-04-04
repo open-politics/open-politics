730673933beb9e36df3b2c1ca490a84205b620e5
Jim Wagner <jimvwagner@googlemail.com>
2024-04-04 15:26:15 +0200

    updates

--- open_politics_project/news/templatetags/custom_filters.py
+++ open_politics_project/news/templatetags/custom_filters.py
@@ -6,3 +6,8 @@ register = template.Library()
 def get_attribute(value, arg):
     default = ""
     return getattr(value, arg, default)
+
+    
+@register.filter(name='has_group')
+def has_group(user, group_name):
+    return user.groups.filter(name=group_name).exists()
