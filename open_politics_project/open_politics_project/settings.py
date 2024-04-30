9027bc903ee07290a9125cc8f04bc8ac33a96311
Jim Wagner <jimvwagner@googlemail.com>
2024-04-30 19:02:47 +0200

    guide integration fix

--- open_politics_project/open_politics_project/settings.py
+++ open_politics_project/open_politics_project/settings.py
@@ -174,10 +174,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_root')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'open_politics_project/news/static/media/')
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_root')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
