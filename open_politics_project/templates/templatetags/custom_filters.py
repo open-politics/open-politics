08f7bac4293d0052d93b779e489a6d3d884cf600
Jim Wagner <jimvwagner@googlemail.com>
2024-05-01 03:16:12 +0200

    user guide added and blog also

--- open_politics_project/templates/templatetags/custom_filters.py
+++ open_politics_project/templates/templatetags/custom_filters.py
@@ -2,11 +2,9 @@ from django import template
 
 register = template.Library()
 
-@register.filter(name='getattr')
-def get_attribute(value, arg):
-    default = ""
-    return getattr(value, arg, default)
-
+@register.filter
+def getattr(obj, attr):
+    return getattr(obj, attr)
     
 @register.filter(name='has_group')
 def has_group(user, group_name):
