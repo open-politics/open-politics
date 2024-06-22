90d623fa642d3510434825d07f274be25aaa7c30
JimVincentw <jimvwagner@googlemail.com>
2024-06-22 20:57:26 +0200

    backend/ proxy/ server configurations

--- backend/app/alembic/env.py
+++ backend/app/alembic/env.py
@@ -82,7 +82,7 @@ def run_migrations_online():
             context.run_migrations()
 
 
-# if context.is_offline_mode():
-#     run_migrations_offline()
-# else:
-#     run_migrations_online()
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
--- backend/app/core/db.py
+++ backend/app/core/db.py
@@ -1,31 +1,29 @@
-from sqlmodel import Session, create_engine, select, SQLModel
+from sqlmodel import Session, create_engine, select
+
 from app import crud
 from app.core.config import settings
 from app.models import User, UserCreate
 
-# Create the database engine
 engine = create_engine(str(settings.SQLALCHEMY_DATABASE_URI))
 
+
+# make sure all SQLModel models are imported (app.models) before initializing DB
+# otherwise, SQLModel might fail to initialize relationships properly
+# for more details: https://github.com/tiangolo/full-stack-fastapi-template/issues/28
+
+
 def init_db(session: Session) -> None:
-    # Drop all tables if they exist and then recreate them
-    SQLModel.metadata.drop_all(engine)
-    SQLModel.metadata.create_all(engine)
+    # Tables should be created with Alembic migrations
+    # But if you don't want to use migrations, create
+    # the tables un-commenting the next lines
+    # from sqlmodel import SQLModel
 
-    # Check if the superuser exists
-    superuser = session.exec(
-        select(User).where(User.email == settings.FIRST_SUPERUSER)
-    ).first()
+    # from app.core.engine import engine
+    # This works because the models are already imported and registered from app.models
+    # print("Creating tables")
+    # SQLModel.metadata.drop_all(engine)
+    # SQLModel.metadata.create_all(engine)
 
-    # If the superuser doesn't exist, create one
-    if not superuser:
-        superuser_data = UserCreate(
-            email=settings.FIRST_SUPERUSER,
-            password=settings.FIRST_SUPERUSER_PASSWORD,
-            is_superuser=True,
-        )
-        crud.create_user(session=session, user_create=superuser_data)
-
-        print("Superuser created.")
     user = session.exec(
         select(User).where(User.email == settings.FIRST_SUPERUSER)
     ).first()
@@ -36,10 +34,3 @@ def init_db(session: Session) -> None:
             is_superuser=True,
         )
         user = crud.create_user(session=session, user_create=user_in)
-
-# Open a new session and initialize the database
-with Session(engine) as session:
-    init_db(session)
-    session.commit()  # Commit transactions to make sure all changes are saved
-
-print("Database initialization complete.")
