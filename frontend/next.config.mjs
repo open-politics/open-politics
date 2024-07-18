4e3da425838a3b318b5c98e0c555f31b73d4a3c9
Robin Janke <mail@robin-janke.de>
2024-07-18 16:12:23 +0200

    update to 1.1.7

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -9,6 +9,7 @@ const __dirname = dirname(__filename);
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+  output: "standalone",
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
