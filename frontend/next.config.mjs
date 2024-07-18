f9f24812a08956be505f08993960f9909b318e69
Robin Janke <mail@robin-janke.de>
2024-07-18 14:09:00 +0200

    fixed frontend (1.0.9)

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -8,58 +8,60 @@ const __dirname = dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
-  images: {
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: '**',
-      },
-      {
-        protocol: 'http',
-        hostname: '**',
-      },
-    ],
-    path: '/_next/image',
-    loader: 'default',
-    loaderFile: '',
-    disableStaticImages: false,
-    minimumCacheTTL: 60,
-    formats: ['image/webp'],
-    dangerouslyAllowSVG: false,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
-    contentDispositionType: 'attachment',
-    unoptimized: false,
-  },
-  rewrites: async () => {
-    return [
-      {
-        source: "/api/:path*",
-        destination:
-            process.env.BACKEND_DOMAIN != "" ? `https://${process.env.BACKEND_DOMAIN}/api/:path*` : "http://backend:80/api/:path*"
-      }
-    ];
-  },
-  webpack: (config, {isServer}) => {
-    config.resolve.alias['@'] = resolve(__dirname, 'src');
-    return config;
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+    pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+    images: {
+        deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+        imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: '**',
+            },
+            {
+                protocol: 'http',
+                hostname: '**',
+            },
+        ],
+        path: '/_next/image',
+        loader: 'default',
+        loaderFile: '',
+        disableStaticImages: false,
+        minimumCacheTTL: 60,
+        formats: ['image/webp'],
+        dangerouslyAllowSVG: false,
+        contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+        contentDispositionType: 'attachment',
+        unoptimized: false,
+    },
+    rewrites: async () => {
+        return [
+            {
+                source: "/api/:path*",
+                destination:
+                    process.env.NODE_ENV === "development"
+                        ? "http://backend:80/api/:path*"
+                        : process.env.NEXT_PUBLIC_API_URL + "/api/:path*",
+            }
+        ];
+    },
+    webpack: (config, {isServer}) => {
+        config.resolve.alias['@'] = resolve(__dirname, 'src');
+        return config;
+    },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
 };
 
 const withMDX = createMDX({
-  // Add markdown plugins here, if needed
-  options: {
-    remarkPlugins: [remarkGfm],
-    rehypePlugins: [],
-  },
-  experimental: {
-    mdxRs: true,
-  },
+    // Add markdown plugins here, if needed
+    options: {
+        remarkPlugins: [remarkGfm],
+        rehypePlugins: [],
+    },
+    experimental: {
+        mdxRs: true,
+    },
 });
 
 // Merge and export the final configuration
