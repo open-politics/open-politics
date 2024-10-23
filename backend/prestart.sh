eba192ee5cf75a979857bfc02825c9c7358202f5
JimVincentW <jimvwagner@googlemail.com>
2024-10-23 12:10:27 +0000

    Deleted: Tests for Geojson

--- backend/prestart.sh
+++ backend/prestart.sh
@@ -6,7 +6,5 @@ python /app/app/backend_pre_start.py
 # Run migrations
 alembic upgrade head
 
-pytest /app/tests/search/test_search_and_geojson.py
-
 # Create initial data in DB
 python /app/app/initial_data.py
