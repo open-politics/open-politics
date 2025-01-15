2dca9e5767bc75d041063a1acb1c928afa433e87
Jim Wagner <jimvwagner@googlemail.com>
2025-01-15 18:42:40 +0000

    Helm/ K8s Webapp Deployment

--- charts/install-k3s.sh
+++ charts/install-k3s.sh
@@ -1,21 +1,40 @@
 #!/bin/bash
 
+# Path: open-politics/charts/setup.sh
+set -e
+
 # Update and upgrade the system
 sudo apt update && sudo apt upgrade -y
 
+# Install prerequisites
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+
 # Install Docker
-sudo apt install -y docker.io
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
 sudo systemctl enable docker
 sudo systemctl start docker
 
-# Install k3s
-curl -sfL https://get.k3s.io | sh -
+# Install k3s with proper kubeconfig permissions
+curl -sfL https://get.k3s.io | sh -s - --write-kubeconfig-mode 644
 
-# Verify k3s installation
-sudo k3s kubectl get nodes
+# Wait for k3s to be ready
+sleep 10
+
+# Configure kubectl for current user
+mkdir -p ~/.kube
+sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
+sudo chown $(id -u):$(id -g) ~/.kube/config
 
 # Install Helm
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 
-# Set up kubectl symlink for easier access
-sudo ln -s /usr/local/bin/k3s /usr/local/bin/kubectl
+# Add required Helm repositories
+helm repo add jetstack https://charts.jetstack.io
+helm repo add kong https://charts.konghq.com
+helm repo update
+
+# Run the boot.sh script to deploy the application
+bash open-politics/charts/boot.sh
